b55e0419495d56e9e66b9520a899c75d34a9c49d
purnale <bhaktipurnale89@gmail.com>
2025-03-12 12:34:21 +0530

    add

--- src/Components/Mainpage.jsx
+++ src/Components/Mainpage.jsx
@@ -1,33 +1,41 @@
 import React, { useState } from "react";
 import Mealcards from "./Mealcards";
+import { LoaderCircle } from "lucide-react";
 
 const Mainpage = () => {
   const [data, setData] = useState();
+  const [loading, setLoading] = useState(false);
   const [search, setSearch] = useState("");
   const [msg, setMsg] = useState("");
 
   const handleInput = (event) => {
     setSearch(event.target.value);
-  }
+  };
 
   const myFun = async () => {
-    if(search == ""){
-     setMsg("Please Enter Someting");
+    if (search == "") {
+      setMsg("Please Enter Someting");
     } else {
-        const get = await fetch(
-            `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
-          );
-          const jsonData = await get.json();
-          setData(jsonData.meals);
-          setMsg("");
+      setLoading(true);
+      const get = await fetch(
+        `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+      );
+      const jsonData = await get.json();
+      setData(jsonData.meals);
+      setMsg("");
+      setLoading(false);
     }
- 
-  }
+  };
 
-
+  if (loading == true)
+    return (
+      <p className="flex items-center justify-center h-screen">
+        <LoaderCircle className="w-10 h-10 animate-spin" />
+      </p>
+    );
   return (
     <>
-    <h1 className="text-center">FOOD RECIPE APP</h1>
+      <h1 className="text-center my-5 text-4xl font-semibold">FOOD RECIPE APP</h1>
       <div>
         <div className="flex justify-center mt-5">
           <input
@@ -45,7 +53,11 @@ const Mainpage = () => {
         </div>
         <h4 className="text-center mt-4 text-red-600">{msg}</h4>
         <div>
-            <Mealcards detail={data}/>
+          {data === null ? (
+            <p>No dish available</p>
+          ) : (
+            <Mealcards detail={data} />
+          )}
         </div>
       </div>
     </>
--- src/Components/Mealinfo.jsx
+++ src/Components/Mealinfo.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from "react";
+import { LoaderCircle } from "lucide-react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 const Mealinfo = () => {
+  const [loading, setLoading] = useState(false);
   const { mealid } = useParams();
   const [info, setInfo] = useState();
   console.log(mealid);
 
   const getInfo = async () => {
+    setLoading(true);
+
     const get = await fetch(
       `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealid}`
     );
     const jsonData = await get.json();
     console.log(jsonData.meals[0]);
     setInfo(jsonData.meals[0]);
+
+    setLoading(false);
   };
 
-  if (info != "") {
+  useEffect(() => {
     getInfo();
-  }
+  }, []);
+
+  if (loading == true)
+    return (
+      <p className="flex items-center justify-center h-screen">
+        <LoaderCircle className="w-10 h-10 animate-spin" />
+      </p>
+    );
 
   return (
     <div className="m-10 border p-10 rounded-2xl bg-blue-100">
       {!info ? (
         "Data Not Found"
       ) : (
-        <div className="flex md:flex-row flex-col justify-center items-center gap-10">
+        <div className="flex md:flex-row flex-col justify-center items-center gap-10 ">
           <img className="w-96 h-96 rounded-2xl" src={info.strMealThumb} />
-          <div className="flex flex-col items-center md:items-start justify-center md:-mt-40">
+          <div className="flex flex-col items-center md:items-start justify-center  ">
             <h1 className=" font-bold text-4xl my-4">
               Recipe Detail For :{" "}
               <span className="text-orange-600"> {info.strMeal}</span>
